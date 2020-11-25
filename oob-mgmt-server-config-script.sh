596ce4284632ffa09074a6f062d804140f1ca344
Justin Betz <44002142+jubetz@users.noreply.github.com>
2020-11-25 09:39:55 -0800

    Update oob-mgmt-server-config-script.sh

--- oob-mgmt-server-config-script.sh
+++ oob-mgmt-server-config-script.sh
@@ -3,20 +3,23 @@
 cat <<EOT > /etc/network/interfaces
 auto lo
 iface lo inet loopback
-  address 10.2.0.132/32
-  address 10.2.0.133/32
+
   
 auto eth0
 iface eth0 inet dhcp
     post-up sysctl -w net.ipv6.conf.eth0.accept_ra=2
 
+
 auto eth1
 iface eth1 inet static
     address 10.22.0.1/24
-    
+  
+  
 auto eth1.1
 iface eth1.1 inet static
     address 10.2.0.10/16
+    address 10.2.0.132/16
+    address 10.2.0.133/16
         
 EOT
 
